f5d300694ecb64d62100e45312d582cdd151e45f
ArthurdeOliveira <arthur.oliveira@autaza.com>
2023-08-23 16:48:44 -0300

    Criação de transação

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,12 +1,28 @@
 import { FastifyInstance } from "fastify"
 import { knex } from "../database"
+import { z } from 'zod'
+import { randomUUID } from "crypto"
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
-    app.get('/hello', async() => {
-        const tables = await knex('sqlite_schema').select('*')
-    
-        return tables
+    app.post('/', async(request, reply) => {
+        
+       
+        const createTransactionBodySchema = z.object( {
+            title: z.string(),
+            amount: z.number(),
+            type: z.enum(['credit', 'debit'])
+        })
+
+        const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+
+        await knex('transactions').insert({
+            id: randomUUID(),
+            title,
+            amount: type == 'credit' ? amount : amount * -1
+        })
+
+        return reply.status(201).send()
     })
 }
