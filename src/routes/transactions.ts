16b6090fa00c78147856de5499860d1ad07fe640
ArthurdeOliveira <arthur.oliveira@autaza.com>
2023-08-23 09:55:35 -0300

    plugins fastify

---
+++ src/routes/transactions.ts
@@ -0,0 +1,12 @@
+import { FastifyInstance } from "fastify"
+import { knex } from "../database"
+
+
+export async function transactionsRoutes(app: FastifyInstance) {
+
+    app.get('/hello', async() => {
+        const tables = await knex('sqlite_schema').select('*')
+    
+        return tables
+    })
+}
