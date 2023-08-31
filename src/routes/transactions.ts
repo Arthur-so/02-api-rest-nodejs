5ddae5e2c879df5c14af470ccb72cf34b2cc35b6
ArthurdeOliveira <arthur.oliveira@autaza.com>
2023-08-31 18:44:12 -0300

    Resumo das transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,13 @@ import { randomUUID } from "crypto"
 
 export async function transactionsRoutes(app: FastifyInstance) {
 
+    app.get('/summary', async () => {
+        const summary = await knex('transactions').sum('amount', 
+        { as: 'amount' }).first()
+
+        return { summary }
+    })
+
     app.get('/:id', async (request) => {
         const getTransactionParamsSchema = z.object({ id: z.string().uuid() })
 
